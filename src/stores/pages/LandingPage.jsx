873906929504e4e00e036bc3ff6abdec38f42ffc
Mansoor-P <munaf6007@gmail.com>
2024-06-27 12:22:31 +0530

    all fashion dresses and products are listed in landing page

--- src/stores/pages/LandingPage.jsx
+++ src/stores/pages/LandingPage.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import ProductLinks from "../components/Navbar/ProductLinks";
 import Footer from "../components/Footer/Footer";
+import Products from "../components/Products/Products";
 
 const LandingPage = () => {
   return (
     <>
       <Navbar />
       <ProductLinks />
+      <Products/>
       <Footer />
     </>
   );
