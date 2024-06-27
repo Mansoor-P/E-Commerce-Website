f805f5269fc69dc40f5900c745151a97b03ce569
Mansoor-P <munaf6007@gmail.com>
2024-06-27 12:14:34 +0530

    All products listed

---
+++ src/stores/components/Products/Products.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import Men from "./Men/Men";
+import Woman from "./Woman/Woman";
+import Mobiles from "./Mobiles/Mobiles";
+import Computers from "./Computers/Computers";
+import Watch from "./Watch/Watch";
+import Furniture from "./Furniture/Furniture";
+import AC from "./AC/AC";
+import Kitchen from "./Kitchen/Kitchen";
+
+
+
+
+
+const Products = () => {
+  return (
+    <div>
+      <Men />
+      <Woman />
+      <Mobiles />
+      <Computers />
+      <Watch />
+      <Furniture />
+      <AC />
+      <Kitchen />
+    </div>
+  );
+};
+
+export default Products;
