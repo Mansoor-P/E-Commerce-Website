847291749c6b813e1951ae8c81e6d4644216f00f
Mansoor-P <munaf6007@gmail.com>
2024-07-04 15:17:12 +0530

    ui updated

--- src/stores/components/Products/ProductComponent.jsx
+++ src/stores/components/Products/ProductComponent.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ProductComponent = ({ categoryData, categoryName,routeTo }) => {
+const ProductComponent = ({ categoryData, categoryName, routeTo }) => {
   const firstFiveImages = categoryData.slice(0, 4);
 
   return (
     <div className="container mx-auto">
       <div className="py-8">
-        <h2 className="text-2xl font-bold text-center mb-4">{categoryName}</h2>
+        <h2 className="text-2xl font-bold  mb-4">{categoryName}</h2>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {firstFiveImages.map((item) => (
-            <div key={item.id}>
-              {/* <h2>{categoryName}</h2> */}
+          {firstFiveImages.map((item, index) => (
+            <div key={`${item.id}-${index}`}>
               <Link to={`/${routeTo}`}>
-                <img className="w-full h-auto rounded-lg" src={item.image} alt="" />
+                <img className="w-full h-auto rounded-lg zoom-hover" src={item.image} alt="" />
               </Link>
             </div>
           ))}
