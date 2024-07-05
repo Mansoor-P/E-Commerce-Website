4cd821b7b210747c493a12cc8bf84e9f9cf4203d
Mansoor-P <munaf6007@gmail.com>
2024-07-05 12:32:17 +0530

    landing page updated

--- src/stores/pages/LandingPage.jsx
+++ src/stores/pages/LandingPage.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar/Navbar";
 import ProductLinks from "../components/Navbar/ProductLinks";
 import Footer from "../components/Footer/Footer";
 import Products from "../components/Products/Products";
+import Carousel from "../components/Carousel";
 
 const LandingPage = () => {
   return (
     <>
       <Navbar />
       <ProductLinks />
+      <Carousel/>
       <Products/>
       <Footer />
     </>
