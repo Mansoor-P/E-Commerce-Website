65c9bdf48cd49a1e1cb42dbfccdc790ec48e0d30
Mansoor-P <munaf6007@gmail.com>
2024-07-06 22:39:27 +0530

    font awsome icons added

--- src/stores/components/Navbar/Navbar.jsx
+++ src/stores/components/Navbar/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useCart } from "../../context/CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faSearch,
+  faUser,
+  faShoppingCart,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  const { cartItems } = useCart(); 
+  const { cartItems } = useCart();
 
   return (
     <div className="bg-white shadow-md p-4">
@@ -13,19 +19,31 @@ const Navbar = () => {
         </Link>
 
         <div className="flex items-center space-x-4">
-          <div className="relative">
-            <input 
-              type="text" 
-              placeholder="Search..." 
+          <div className="relative flex items-center">
+            <input
+              type="text"
+              placeholder="Search..."
               className="px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-primary"
+            />
+            <FontAwesomeIcon
+              icon={faSearch}
+              className="absolute right-3 text-gray-500"
             />
           </div>
           <div>
-            <Link to="/signin" className="text-gray-600 hover:text-primary">
+            <Link
+              to="/signin"
+              className="text-gray-600 hover:text-primary flex items-center"
+            >
+              <FontAwesomeIcon icon={faUser} className="mr-2" />
               Sign In / Sign Up
             </Link>
           </div>
-          <Link to="/cart" className="relative flex items-center text-gray-600 hover:text-primary">
+          <Link
+            to="/cart"
+            className="relative flex items-center text-gray-600 hover:text-primary"
+          >
+            <FontAwesomeIcon icon={faShoppingCart} className="mr-2" />
             <span>Cart</span>
             <span className="ml-1 bg-red-500 text-white rounded-full px-2 py-1 text-xs">
               {cartItems.length}
